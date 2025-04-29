3a60303d2a4de99049aed753a6ddb6e62676ab32
Apollo Sa <apollolds2@gmail.com>
2025-04-29 09:45:18 -0300

    Criar_routes.dart #1

---
+++ lib/routes.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'screens/splash_screen.dart';
+import 'screens/login_page.dart';
+import 'screens/home_page.dart';
+import 'screens/cadastro/cadastro_obra_page.dart';
+import 'screens/cadastro/cadastro_cliente_page.dart';
+import 'screens/cadastro/cadastro_servico_page.dart';
+import 'screens/obra_detalhe_page.dart';
+
+final Map<String, WidgetBuilder> appRoutes = {
+  '/': (context) => SplashScreen(),
+  '/login': (context) => LoginPage(),
+  '/home': (context) => HomePage(),
+  '/cadastro/obra': (context) => CadastroObraPage(),
+  '/cadastro/cliente': (context) => CadastroClientePage(),
+  '/cadastro/servico': (context) => CadastroServicoPage(),
+  '/obra/detalhe': (context) => ObraDetalhePage(),
+};
